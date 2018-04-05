1fc759a55cf29993d51a2cbe2f9da66689d19fdb
fo2rist <fo2@inbox.ru>
2018-04-05 00:06:45 -0700

    Extract Race Calendar and its Loader to dedicated classes

---
+++ app/src/main/java/com/github/fo2rist/mclaren/ui/calendar/RaceCalendar.java
@@ -0,0 +1,7 @@
+package com.github.fo2rist.mclaren.ui.calendar;
+
+import com.github.fo2rist.mclaren.ui.models.CalendarEvent;
+import java.util.ArrayList;
+
+public class RaceCalendar extends ArrayList<CalendarEvent> {
+}
