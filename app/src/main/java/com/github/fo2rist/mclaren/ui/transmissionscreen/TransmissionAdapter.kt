569d1d1259fab97f9527a8a1508a03d069809a95
fo2rist <fo2@inbox.ru>
2018-04-08 23:26:26 -0700

    Add porgress bard and titles to Transmission Screen

--- app/src/main/java/com/github/fo2rist/mclaren/ui/transmissionscreen/TransmissionAdapter.kt
+++ app/src/main/java/com/github/fo2rist/mclaren/ui/transmissionscreen/TransmissionAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.fo2rist.mclaren.ui.transmissionscreen
 
+import android.support.annotation.StringRes
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.github.fo2rist.mclaren.R
 import com.github.fo2rist.mclaren.models.TransmissionItem
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.DRIVER_A_TO_PIT
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.DRIVER_B_TO_PIT
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.MESSAGE_ABOUT_DRIVER_A
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.MESSAGE_ABOUT_DRIVER_B
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.MESSAGE_GENERAL
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.PIT_TO_DRIVER_A
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.PIT_TO_DRIVER_B
+import com.github.fo2rist.mclaren.models.TransmissionItem.Type.PIT_TO_EVERYONE
 
 class TransmissionAdapter(
         private val items: ArrayList<TransmissionItem> = ArrayList()
@@ -14,10 +23,30 @@ class TransmissionAdapter(
 
     class TransmissionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val messageText = view.findViewById<TextView>(R.id.message_text)
+        val messageTitle = view.findViewById<TextView>(R.id.title_text)
 
         fun displayItem(item: TransmissionItem) {
             messageText.text = item.message
+            messageTitle.text = getTitleFor(item)
         }
+
+        private fun getTitleFor(item: TransmissionItem): CharSequence {
+            return when(item.type) {
+                DRIVER_A_TO_PIT, DRIVER_B_TO_PIT ->
+                    item.driverName ?: ""
+                PIT_TO_DRIVER_A, PIT_TO_DRIVER_B ->
+                    getString(R.string.title_pit_to_racer_format, item.driverName ?: "")
+                PIT_TO_EVERYONE ->
+                    getString(R.string.title_pitwall)
+                MESSAGE_ABOUT_DRIVER_A, MESSAGE_ABOUT_DRIVER_B, MESSAGE_GENERAL ->
+                    getString(R.string.title_announcement)
+                else ->
+                    ""
+            }
+        }
+
+        private fun getString(@StringRes stringId: Int, vararg formatArgs: String) =
+                itemView.context.getString(stringId, *formatArgs)
     }
 
     override fun getItemCount(): Int {
