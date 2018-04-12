f672554824d6af91bca281bc3588779c7fc3e7f1
fo2rist <fo2@inbox.ru>
2018-04-11 22:52:34 -0700

    Add Driver/Pit Avatars and proper alignment to Transmission screen

--- app/src/main/java/com/github/fo2rist/mclaren/ui/transmissionscreen/TransmissionAdapter.kt
+++ app/src/main/java/com/github/fo2rist/mclaren/ui/transmissionscreen/TransmissionAdapter.kt
@@ -2,9 +2,11 @@ package com.github.fo2rist.mclaren.ui.transmissionscreen
 
 import android.support.annotation.StringRes
 import android.support.v7.widget.RecyclerView
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.github.fo2rist.mclaren.R
 import com.github.fo2rist.mclaren.models.TransmissionItem
@@ -24,14 +26,39 @@ class TransmissionAdapter(
     class TransmissionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val messageText = view.findViewById<TextView>(R.id.message_text)
         val messageTitle = view.findViewById<TextView>(R.id.title_text)
+        val driverImage = view.findViewById<ImageView>(R.id.driver_avatar_image)
+        val pitwallImage = view.findViewById<ImageView>(R.id.pit_avatar_image)
 
         fun displayItem(item: TransmissionItem) {
-            messageText.text = item.message
-            messageTitle.text = getTitleFor(item)
+            setMessageFor(item)
+            setTitleFor(item)
+            setImagesFor(item)
+            alignTextFor(item)
         }
 
-        private fun getTitleFor(item: TransmissionItem): CharSequence {
-            return when(item.type) {
+        private fun alignTextFor(item: TransmissionItem) {
+            when (item.type) {
+                DRIVER_A_TO_PIT, DRIVER_B_TO_PIT -> {
+                    messageTitle.gravity = Gravity.RIGHT
+                    messageText.gravity = Gravity.RIGHT
+                }
+                PIT_TO_DRIVER_A, PIT_TO_DRIVER_B -> {
+                    messageTitle.gravity = Gravity.LEFT
+                    messageText.gravity = Gravity.LEFT
+                }
+                else -> {
+                    messageTitle.gravity = Gravity.CENTER_HORIZONTAL
+                    messageText.gravity = Gravity.LEFT
+                }
+            }
+        }
+
+        private fun setMessageFor(item: TransmissionItem) {
+            messageText.text = item.message
+        }
+
+        private fun setTitleFor(item: TransmissionItem) {
+            messageTitle.text = when (item.type) {
                 DRIVER_A_TO_PIT, DRIVER_B_TO_PIT ->
                     item.driverName ?: ""
                 PIT_TO_DRIVER_A, PIT_TO_DRIVER_B ->
@@ -40,8 +67,29 @@ class TransmissionAdapter(
                     getString(R.string.title_pitwall)
                 MESSAGE_ABOUT_DRIVER_A, MESSAGE_ABOUT_DRIVER_B, MESSAGE_GENERAL ->
                     getString(R.string.title_announcement)
-                else ->
-                    ""
+            }
+        }
+
+        private fun setImagesFor(item: TransmissionItem) {
+            driverImage.setImageResource(when (item.type) {
+                DRIVER_A_TO_PIT -> R.drawable.driver_alonso
+                DRIVER_B_TO_PIT -> R.drawable.driver_vandoorne
+                else -> 0
+            })
+
+            when (item.type) {
+                DRIVER_A_TO_PIT, DRIVER_B_TO_PIT -> {
+                    driverImage.visibility = View.VISIBLE
+                    pitwallImage.visibility = View.INVISIBLE
+                }
+                PIT_TO_DRIVER_A, PIT_TO_DRIVER_B -> {
+                    driverImage.visibility = View.INVISIBLE
+                    pitwallImage.visibility = View.VISIBLE
+                }
+                else -> {
+                    driverImage.visibility = View.GONE
+                    pitwallImage.visibility = View.GONE
+                }
             }
         }
 
