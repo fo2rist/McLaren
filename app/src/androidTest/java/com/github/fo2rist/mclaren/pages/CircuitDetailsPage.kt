6b29ed881272bdfb42feb1bb9208a4b1a1642863
fo2rist <fo2@inbox.ru>
2018-10-07 23:26:26 -0700

    MIgrate Circuit Details Fragment test to Kakao

---
+++ app/src/androidTest/java/com/github/fo2rist/mclaren/pages/CircuitDetailsPage.kt
@@ -0,0 +1,26 @@
+package com.github.fo2rist.mclaren.pages
+
+import android.support.annotation.StringRes
+import com.agoda.kakao.KImageView
+import com.agoda.kakao.KTextView
+import com.agoda.kakao.Screen
+import com.github.fo2rist.mclaren.R
+
+class CircuitDetailsPage : Screen<CircuitDetailsPage>() {
+    val title = KTextView { withId(R.id.circuit_title) }
+
+    val circuitImage = KImageView{ withId(R.id.circuit_image) }
+
+    val circuitDetails = KTextView { withId(R.id.circuit_details)}
+
+    val detailsItemLaps = detailsItem(R.string.circuit_details_laps)
+    val detailsItemLength = detailsItem(R.string.circuit_details_length)
+    val detailsItemDistance = detailsItem(R.string.circuit_details_distance)
+    val detailsItemSeasons = detailsItem(R.string.circuit_details_seasons)
+    val detailsItemGpHeld = detailsItem(R.string.circuit_details_gp_held)
+
+    private fun detailsItem(@StringRes title: Int) = KTextView {
+        withId(R.id.value)
+        withSibling { withText(title) }
+    }
+}
