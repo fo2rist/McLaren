6d584d47088a5c33108b98ad776295d57c998537
fo2rist <fo2@inbox.ru>
2018-10-28 14:22:50 -0700

    Improve intents interception in Main Actitivity test and Circuits Fragment test
    Intercept all intents and add automated interceptions cancellation to the base test class

--- app/src/androidTest/java/com/github/fo2rist/mclaren/tests/BaseMainActivityTest.kt
+++ app/src/androidTest/java/com/github/fo2rist/mclaren/tests/BaseMainActivityTest.kt
@@ -1,12 +1,16 @@
 package com.github.fo2rist.mclaren.tests
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import android.content.Context
+import android.content.Intent
 import android.support.annotation.CallSuper
+import android.support.test.espresso.intent.Intents
 import android.support.test.rule.ActivityTestRule
-
 import com.github.fo2rist.mclaren.pages.MainPage
-
 import com.github.fo2rist.mclaren.ui.MainActivity
+import org.hamcrest.Matchers.any
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 
@@ -23,5 +27,43 @@ open class BaseMainActivityTest {
     open fun setUp() {
         context = activityRule.activity
         mainPage = MainPage()
+    }
+
+    @After
+    @CallSuper
+    fun tearDown() {
+        endIntentsInterception()
+    }
+
+    private var intentInterceptionActive = false
+
+    /**
+     * Start intercepting all intents and responding with given result.
+     * Only active until current test is finished with [tearDown].
+     */
+    protected fun interceptIntents(responseResult: Int = Activity.RESULT_OK) {
+        if (intentInterceptionActive) {
+            return
+        }
+
+        intentInterceptionActive = true
+        Intents.init()
+        Intents.intending(any(Intent::class.java))
+                .respondWith(ActivityResult(responseResult, null))
+    }
+
+    /** Stop interception intents. */
+    protected fun endIntentsInterception() {
+        if (!intentInterceptionActive) {
+            return
+        }
+
+        try {
+            Intents.release()
+        } catch (exc: Exception) {
+            //we don't really care
+        } finally {
+            intentInterceptionActive = false
+        }
     }
 }
