13c2c417463dd4637a13bf16339bb82e81e42c50
fo2rist <fo2@inbox.ru>
2018-10-28 18:41:21 -0700

    Migrate Drivers Fragment test to Kakao and add more checks

---
+++ app/src/androidTest/java/com/github/fo2rist/mclaren/pages/DriversPage.kt
@@ -0,0 +1,41 @@
+package com.github.fo2rist.mclaren.pages
+
+
+import com.agoda.kakao.KTextView
+import com.agoda.kakao.KView
+import com.agoda.kakao.KViewPager
+import com.agoda.kakao.Screen
+import com.github.fo2rist.mclaren.R
+import com.github.fo2rist.mclaren.ui.widgets.InformationLineView
+
+class DriversPage : Screen<DriversPage>() {
+
+    fun swipeLeftAndWait() {
+        viewPager.swipeLeft()
+        Thread.sleep(600) // typical swipe duration
+    }
+
+    private val viewPager = KViewPager { withId(R.id.drivers_pager) }
+
+    val driverNumber = KTextView {
+        withParent { isDisplayed() }
+        withId(R.id.driver_number_text)
+    }
+
+    val driverPageLink = KView {
+        withParent { isDisplayed() }
+        withId(R.id.team_link_button)
+    }
+
+    fun infoItemWithTitle(title: String) = KTextView {
+        withId(R.id.value)
+        withParent {
+            isInstanceOf(InformationLineView::class.java)
+            withParent { isDisplayed() }
+        }
+        withSibling {
+            withId(R.id.title)
+            withText(title)
+        }
+    }
+}
