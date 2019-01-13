8a5109c2c9c98dda80c32db8b5a7a6dbafd161b1
fo2rist <fo2@inbox.ru>
2019-01-12 22:13:49 -0800

    Set explicit SDK for StoryStrea web serivce test

--- app/src/test/java/com/github/fo2rist/mclaren/web/StoryStreamWebServiceImplTest.kt
+++ app/src/test/java/com/github/fo2rist/mclaren/web/StoryStreamWebServiceImplTest.kt
@@ -3,8 +3,10 @@ package com.github.fo2rist.mclaren.web
 import org.junit.Before
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21])
 class StoryStreamWebServiceImplTest : BaseFeedWebServiceTest() {
 
     override lateinit var webservice: BaseFeedWebService
