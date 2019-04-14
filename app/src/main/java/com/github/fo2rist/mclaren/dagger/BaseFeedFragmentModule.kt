d743fde5f7aee24396f89306fb3dda8651cfce31
fo2rist <fo2@inbox.ru>
2019-04-13 22:33:56 -0700

    Simplify feed presenter injection config StoryStream and McLarenFeed modules now bind corresponding fragment and repo and base module provides the same presenter class using these parameters

---
+++ app/src/main/java/com/github/fo2rist/mclaren/dagger/BaseFeedFragmentModule.kt
@@ -0,0 +1,14 @@
+package com.github.fo2rist.mclaren.dagger
+
+import com.github.fo2rist.mclaren.mvp.FeedContract
+import com.github.fo2rist.mclaren.ui.feedscreen.FeedPresenter
+import dagger.Binds
+import dagger.Module
+
+@Module
+interface BaseFeedFragmentModule {
+
+    @Binds
+    @Scopes.PerFragment
+    fun providePresenter(presenter: FeedPresenter): FeedContract.Presenter
+}
--- app/src/main/java/com/github/fo2rist/mclaren/dagger/McLarenFeedFragmentModule.kt
+++ app/src/main/java/com/github/fo2rist/mclaren/dagger/McLarenFeedFragmentModule.kt
@@ -1,30 +1,20 @@
 package com.github.fo2rist.mclaren.dagger
 
-import com.github.fo2rist.mclaren.analytics.EventsLogger
 import com.github.fo2rist.mclaren.mvp.FeedContract
 import com.github.fo2rist.mclaren.repository.FeedRepository
-import com.github.fo2rist.mclaren.repository.FeedRepositoryEventBus
-import com.github.fo2rist.mclaren.ui.feedscreen.FeedPresenter
 import com.github.fo2rist.mclaren.ui.feedscreen.McLarenFeedFragment
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import javax.inject.Named
 
-@Module
-internal class McLarenFeedFragmentModule {
+@Module(includes = [BaseFeedFragmentModule::class])
+internal interface McLarenFeedFragmentModule {
 
-    @Provides
+    @Binds
     @Scopes.PerFragment
-    fun provideMcLarenFeedView(view: McLarenFeedFragment): FeedContract.View = view
+    fun provideMcLarenFeedView(view: McLarenFeedFragment): FeedContract.View
 
-    @Provides
+    @Binds
     @Scopes.PerFragment
-    fun providePresenter(
-        view: FeedContract.View,
-        @Named("mclaren-feed") repository: FeedRepository,
-        eventBus: FeedRepositoryEventBus,
-        logger: EventsLogger
-    ): FeedContract.Presenter {
-        return FeedPresenter(view, repository, eventBus, logger)
-    }
+    fun provideMcLarenFeedRepository(@Named("mclaren-feed") repository: FeedRepository): FeedRepository
 }
