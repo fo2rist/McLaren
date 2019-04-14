623a0e978c3129199ae16e5143ccfbe0c376435e
fo2rist <fo2@inbox.ru>
2019-04-13 22:33:56 -0700

    Simplify feed presenter injection config StoryStream and McLarenFeed modules now bind corresponding fragment and repo and base module provides the same presenter class using these parameters

--- app/src/main/java/com/github/fo2rist/mclaren/dagger/StoriesFragmentModule.kt
+++ app/src/main/java/com/github/fo2rist/mclaren/dagger/StoriesFragmentModule.kt
@@ -1,30 +1,20 @@
 package com.github.fo2rist.mclaren.dagger
 
-import com.github.fo2rist.mclaren.analytics.EventsLogger
 import com.github.fo2rist.mclaren.mvp.FeedContract
 import com.github.fo2rist.mclaren.repository.FeedRepository
-import com.github.fo2rist.mclaren.repository.FeedRepositoryEventBus
-import com.github.fo2rist.mclaren.ui.feedscreen.FeedPresenter
 import com.github.fo2rist.mclaren.ui.feedscreen.StoriesFeedFragment
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import javax.inject.Named
 
-@Module
-internal class StoriesFragmentModule {
+@Module(includes = [BaseFeedFragmentModule::class])
+internal interface StoriesFragmentModule {
 
-    @Provides
+    @Binds
     @Scopes.PerFragment
-    fun provideStoriesFeedView(view: StoriesFeedFragment): FeedContract.View = view
+    fun provideStoriesFeedView(view: StoriesFeedFragment): FeedContract.View
 
-    @Provides
+    @Binds
     @Scopes.PerFragment
-    fun providePresenter(
-        view: FeedContract.View,
-        @Named("storystream-feed") repository: FeedRepository,
-        eventBus: FeedRepositoryEventBus,
-        logger: EventsLogger
-    ): FeedContract.Presenter {
-        return FeedPresenter(view, repository, eventBus, logger)
-    }
+    fun provideStoryStreamRepository(@Named("storystream-feed") repository: FeedRepository): FeedRepository
 }
