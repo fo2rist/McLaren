240106985cf72abf2931c4549478015a2cd5fa32
fo2rist <fo2@inbox.ru>
2019-12-29 20:40:19 -0800

    Fix IntentUtilsTest Launch base activity to perform intent resolution in UI thread as new Android requires

--- app/src/androidTest/java/com/github/fo2rist/mclaren/tests/utils/IntentUtilsTest.kt
+++ app/src/androidTest/java/com/github/fo2rist/mclaren/tests/utils/IntentUtilsTest.kt
@@ -1,24 +1,29 @@
 package com.github.fo2rist.mclaren.tests.utils
 
+import android.app.Activity
 import android.app.Instrumentation
-import android.content.Context
 import android.content.Intent
-import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.intent.Intents
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasAction
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.github.fo2rist.mclaren.ui.MainActivity
 import com.github.fo2rist.mclaren.utils.IntentUtils
 import org.hamcrest.Matchers.any
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class IntentUtilsTest{
+class IntentUtilsTest {
 
-    private val context: Context by lazy { InstrumentationRegistry.getTargetContext() }
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    private val activity: Activity by lazy { activityRule.activity }
 
     @Before
     fun setUp() {
@@ -29,9 +34,12 @@ class IntentUtilsTest{
 
     @Test
     fun test_openInBrowser_launches_view_intent() {
-        val launched = IntentUtils.openInBrowser(context, "http://dummy.url")
+        activity.runOnUiThread {
+            val launched = IntentUtils.openInBrowser(activity, "http://dummy.url")
 
-        Assert.assertTrue(launched)
+            Assert.assertTrue(launched)
+        }
+
         Intents.intended(hasAction(Intent.ACTION_VIEW))
     }
 
