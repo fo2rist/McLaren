5ef4cc9602f52f564baf4d8476932ed24d3d895d
fo2rist <fo2@inbox.ru>
2020-07-26 16:13:41 -0700

    Migrate Driver and Image Preview fragments tests to androidx

--- app/src/test/java/com/github/fo2rist/mclaren/ui/driversscreen/DriverSubFragmentTest.kt
+++ app/src/test/java/com/github/fo2rist/mclaren/ui/driversscreen/DriverSubFragmentTest.kt
@@ -1,9 +1,10 @@
 package com.github.fo2rist.mclaren.ui.driversscreen
 
-import android.content.Context
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.agoda.kakao.common.views.KView
-import com.agoda.kakao.image.KImageView
+import com.github.fo2rist.mclaren.McLarenApplication
 import com.github.fo2rist.mclaren.R
 import com.github.fo2rist.mclaren.repository.remoteconfig.DriversRepositoryImpl
 import com.github.fo2rist.mclaren.ui.models.Driver
@@ -12,80 +13,72 @@ import com.github.fo2rist.mclaren.ui.models.DriverProperty
 import com.github.fo2rist.mclaren.web.remoteconfig.FirebaseRemoteConfigService
 import com.google.firebase.FirebaseApp
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
+
+// TODO ideally this test can be independent from Firebase if we can inject the drivers repo to the fragment
+private val repository by lazy { DriversRepositoryImpl(FirebaseRemoteConfigService()) }
+
+private val DRIVER_ID = DriverId.SAINZ
+private val DRIVER_MODEL: Driver by lazy { repository.getDriver(DRIVER_ID) }
 
 /**
  * Test view content population for [DriverSubFragment].
  */
-@RunWith(RobolectricTestRunner::class)
-@Ignore("Need to support/avoid injection, simplest option — make class open, override injection and pass mock")
+@RunWith(AndroidJUnit4::class)
 class DriverSubFragmentTest {
-    private val driverForTest = DriverId.SAINZ
-    private lateinit var driverModel: Driver
+    private var app: McLarenApplication = ApplicationProvider.getApplicationContext()
 
-    private var context: Context = RuntimeEnvironment.application
-
-    private lateinit var rootLayout: KView
-    private lateinit var portraitImage: KImageView
 
     @Before
     fun setUp() {
+        // force setting the theme is required for Floating button, for some reason theme from manifest is not applied
+        app.setTheme(R.style.AppTheme)
         // in tests Firebase is not initialized by default, so have to do that manually
-        FirebaseApp.initializeApp(context)
-
-        // TODO ideally this test can be independent from Firebase if we can inject the drivers repo to the fragment
-        //     then test can fully rely on its own test data, instead of full dagger initialization.
-        //     another option would be to pass the full driver model as the parameter to the fragment
-        val driversRepository = DriversRepositoryImpl(FirebaseRemoteConfigService())
-        driverModel = driversRepository.getDriver(driverForTest)
+        FirebaseApp.initializeApp(app)
 
         // here fragment itself is not required, only needed to fetch args bundle from it for launch
-        val fragment = DriverSubFragment.newInstance(driverForTest)
-        launchFragmentInContainer<DriverSubFragment>(fragment.arguments)
-
-        // TODO use launchInContainer(DriverSubFragment.class) instead of old
-        //  FragmentTestUtil.startFragment(fragment, MainActivity.class);
-        //  Robolectric doesn't work well with androidx fragments
-        rootLayout = KView { withId(R.id.properties_linearlayout) }
-        portraitImage = KImageView { withId(R.id.driver_portrait_image) }
+        val fragment = DriverSubFragment.newInstance(DRIVER_ID)
+        launchFragmentInContainer<TestDriverSubFragment>(fragment.arguments)
     }
 
     @Test
     fun test_layout_forActiveDriver_contains_Mandatory_and_Optional_fields() {
-        //Main fields present (both title and value)
-        assertTitleAndValuePresentFor(DriverProperty.Name)
-        assertTitleAndValuePresentFor(DriverProperty.DateOfBirth)
-        assertTitleAndValuePresentFor(DriverProperty.Nationality)
-        assertTitleAndValuePresentFor(DriverProperty.Twitter)
+        val page = KView { withId(R.id.properties_linearlayout) }
 
-        //options fields present
-        assertTitleAndValuePresentFor(DriverProperty.PolePositions)
-        assertTitleAndValuePresentFor(DriverProperty.FastestLaps)
+        // Main fields present (both title and value)
+        page.assertTitleAndValuePresentFor(DriverProperty.Name)
+        page.assertTitleAndValuePresentFor(DriverProperty.DateOfBirth)
+        page.assertTitleAndValuePresentFor(DriverProperty.Nationality)
+        page.assertTitleAndValuePresentFor(DriverProperty.Twitter)
 
-        //tag present
-        rootLayout {
-            hasDescendant {
-                withText(driverModel[DriverProperty.Tag]!!)
-            }
+        // optional fields present
+        page.assertTitleAndValuePresentFor(DriverProperty.PolePositions)
+        page.assertTitleAndValuePresentFor(DriverProperty.FastestLaps)
+
+        // tag present
+        page.assertHasViewWithText(DRIVER_MODEL[DriverProperty.Tag]!!)
+    }
+
+    private fun KView.assertTitleAndValuePresentFor(property: DriverProperty) {
+        hasDescendant {
+            withText(app.getString(property.nameResId))
         }
-        //photo resolved
-        portraitImage {
-            hasDrawable(R.drawable.driver_portrait_sainz)
+        hasDescendant {
+            withText(DRIVER_MODEL[property]!!)
         }
     }
 
-    private fun assertTitleAndValuePresentFor(property: DriverProperty) {
-        rootLayout {
-            hasDescendant {
-                withText(context.getString(property.nameResId))
-            }
-            hasDescendant {
-                withText(driverModel[property]!!)
-            }
+    private fun KView.assertHasViewWithText(text: String) {
+        hasDescendant {
+            withText(text)
+        }
+    }
+
+    // test version of fragment which avoids android dagger and allows manual injection
+    class TestDriverSubFragment : DriverSubFragment() {
+        override fun injectDependencies() {
+            this.driversRepository = repository
         }
     }
 }
