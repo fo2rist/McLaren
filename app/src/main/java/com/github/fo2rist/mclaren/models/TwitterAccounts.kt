d0b7d7baacaf444776996b8ef8d03b8d6e5e9c01
fo2rist <fo2@inbox.ru>
2020-08-16 19:21:18 -0700

    Change object's field from const to JvmField Check how JaCoCo would report that

--- app/src/main/java/com/github/fo2rist/mclaren/models/TwitterAccounts.kt
+++ app/src/main/java/com/github/fo2rist/mclaren/models/TwitterAccounts.kt
@@ -4,5 +4,7 @@ package com.github.fo2rist.mclaren.models
  * Twitter accounts recognized by the app.
  */
 object TwitterAccounts {
-    const val TWITTER_MCLAREN_F1 = "mclarenf1"
+
+    @JvmField
+    val TWITTER_MCLAREN_F1 = "mclarenf1"
 }
